c5fca46735c45b69f9fdc95181e8a6afe35d3458
MAYUR PARMAR <parmar.mayurumb11@gmail.com>
2024-01-23 12:38:07 +0530

    .

---
+++ src/pages/Accounts/Account.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Account() {
+  return (
+    <div>
+      <h1>Account js file</h1>
+    </div>
+  );
+}
---
+++ src/pages/Leads/Leads.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Leads() {
+  return (
+    <div>
+      <h1>Leads js file</h1>
+    </div>
+  );
+}
