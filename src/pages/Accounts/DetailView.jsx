e4cff7400bb7506ca7cdb8d1d6aa82e047d2a045
MAYUR PARMAR <parmar.mayurumb11@gmail.com>
2024-01-30 14:50:50 +0530

    .

---
+++ src/pages/Accounts/DetailView.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function DetailView() {
+  const data = JSON.parse(localStorage.getItem("account"));
+  console.log(data);
+  return (
+    <React.Fragment>
+      <h2>Detail View</h2>
+      <table>
+        <thead>
+          <tr>
+            {data.layoutdefs.detailview.map((element, index) => (
+              <th key={index}>{element}</th>
+            ))}
+          </tr>
+        </thead>
+      </table>
+    </React.Fragment>
+  );
+}
---
+++ src/pages/Contacts/EditView.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function EditView() {
+  const data = JSON.parse(localStorage.getItem("contacts"));
+  console.log(data);
+  return (
+    <React.Fragment>
+      <h2>Edit View</h2>
+      <table>
+        <thead>
+          <tr>
+            {data.layoutdefs.editview.map((element, index) => (
+              <th key={index}>{element}</th>
+            ))}
+          </tr>
+        </thead>
+      </table>
+    </React.Fragment>
+  );
+}
